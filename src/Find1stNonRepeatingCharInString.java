1b70fe09c56ebe31550633ef72a9ffcb3b5bb572
Elads <elad.shoval@imperva.com>
2020-05-11 22:25:48 +0300

    Add some more ...

---
+++ src/Find1stNonRepeatingCharInString.java
@@ -0,0 +1,65 @@
+import java.util.LinkedHashMap;
+
+public class Find1stNonRepeatingCharInString {
+
+//    private static class MyExcpetion extends Exception {
+//        public int index;
+//
+//        public MyExcpetion(int index) {
+//            this.index = index;
+//        }
+//    }
+
+    private static class Entry {
+        public Integer amount;
+        public int index;
+    }
+
+    private static int retIndex;
+
+    public static int findIndex(String str) {
+        if (str == null || str.length() == 0) {
+            return -1;
+        }
+
+        LinkedHashMap<Integer, Entry> chars = new LinkedHashMap<Integer, Entry>();
+
+        for (int i = 0; i < str.length(); i++) {
+            Integer charAsInt = (Integer) new Integer(str.charAt(i));
+            if (chars.get(charAsInt) == null) {
+                Entry e = new Entry();
+                e.amount = 0;
+                chars.put(charAsInt ,e);
+            }
+            Entry e = chars.get(charAsInt);
+            e.amount = e.amount + 1;
+            e.index = i;
+        }
+
+        // for (Map.Entry<Integer, Entry> entry : chars.linkedList()) {
+        synchronized(Find1stNonRepeatingCharInString.class) {
+            retIndex = -1;
+//        try {
+            chars.forEach((k, v) -> {
+                if (v.amount == 1) {
+                    retIndex = v.index;
+//                    throw new MyExcpetion(v.index);
+                }
+            });
+//        } catch (MyExcpetion me) {
+//            ret = me.index;
+//        }
+
+            return retIndex;
+        }
+    }
+
+
+    public static void main(String args[]) {
+
+        String str = "GeeksForGeeks";
+
+        System.out.println("index is - " + findIndex(str));
+        // index 5 expected
+    }
+}
---
+++ src/IsOneArrayRotationAnother.java
@@ -0,0 +1,52 @@
+
+// a = { 0, 0, 1, 2, 0}
+// b = { 0, 1, 2, 0, 0}
+
+public class IsOneArrayRotationAnother {
+
+    public static boolean check(int[] a, int[] b) {
+        if (a == null || b == null || a.length != b.length) {
+            return false;
+        }
+
+        int i = 0, j = 0, lastStart = 0;
+
+        while (j < 2 * b.length) {
+            int k = j % b.length;
+            if (a[i] == b[k]) {
+                i++;
+            } else {
+                i = 0;
+                lastStart++;
+                j = lastStart;
+            }
+
+            if (i == a.length - 1) {
+                return true;
+            }
+
+            j++;
+        }
+
+        return false;
+    }
+
+    public static void main(String args[]) {
+
+        // i = 0
+        // j = 0
+        // k = 0
+
+//        int[] a = { 1, 2 };
+//        int[] b = { 2, 1 };
+
+
+        int[] a = { 0, 0, 1, 2, 0 };
+        int[] b = { 0, 1, 2, 0, 0 };
+
+        //			0, 1, 2, /0, 0, 0, 1, 2, 0/,  0
+
+        System.out.println(check(a, b) ? "found :)" : "not found :(");
+    }
+
+}
