d93259359fd6f84fef64ea3f945586d8f67c717c
Elads <elad.shoval@imperva.com>
2020-07-09 11:48:38 +0300

    Add some more ...

--- src/TestCode.java
+++ src/TestCode.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class TestCode {
 
@@ -15,6 +17,7 @@ public class TestCode {
         String[] strArr = new String[] {"vv" ,"bbb"};
         final int minLen = Arrays.stream(strArr)
                 // .map(String::length)
+                //.sorted(Comparator.comparing(String::length))
                 .min(Comparator.comparing(String::length))
                 .get()
                 .length();
@@ -31,5 +34,48 @@ public class TestCode {
         System.out.println("val 1 - " + val);
         val = iterator.next();
         System.out.println("val 2 - " + val);
+
+        Arrays.sort(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+
+        changeInteger(val);
+        System.out.println("val 3 - " + val);
+
+        String pre = "9,1,2,3,#,#";
+
+        class Temp {
+            Boolean res = false;
+
+            public void setRes(Boolean res) {
+                this.res = res;
+            }
+        }
+
+        Temp temp = new Temp();
+        final String[] split = pre.split(",");
+        Arrays.stream(split).forEach(s -> {
+            temp.setRes(s != "#");
+        });
+
+        System.out.println();
+    }
+
+    public Map<String, String> pairs(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        Arrays.stream(strings)
+                // .map(s -> new javafx.util.Pair<Character, Character>(s[0], s[s.length() -1]))
+//                .collect(
+//                        Collectors.toMap(
+//                                s -> "" + s.charAt(0),
+//                                s -> "" + s.charAt(s.length() - 1)));
+
+                .forEach(s -> {
+                    map.put("" + s.charAt(0), "" + s.charAt(s.length() - 1));
+                });
+        return map;
+    }
+
+    public static void changeInteger(Integer val) {
+        val++;
     }
 }
